c3197e6696fda5500dda7b20c252c2416e2532cb
kok-s0s <2694308562@qq.com>
2024-01-08 20:35:50 +0800

    feat: simple css

--- src/components/RepositoryIntroCard.tsx
+++ src/components/RepositoryIntroCard.tsx
@@ -14,16 +14,18 @@ interface RepositoryIntroCardProps {
 
 const RepositoryIntroCard = ({ repository }: RepositoryIntroCardProps) => {
   return (
-    <div>
-      <a href={repository.link}>{repository.name}</a>
-      <p>{repository.description}</p>
-      <div>
-        {repository.stacks.map((stack) => (
-          <div key={stack}>
-            <SkillBadge skill={stack} />
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-col gap-2 p-4 rounded-md transition-all duration-300 ease-in-out transform group hover:opacity-80 hover:bg-gray-100 hover:shadow-lg hover:shadow-purple-500">
+      <a href={repository.link}>
+        <p className="group-hover:text-purple-500">{repository.name}</p>
+        <p>{repository.description}</p>
+        <div className="flex gap-1">
+          {repository.stacks.map((stack) => (
+            <div key={stack}>
+              <SkillBadge skill={stack} />
+            </div>
+          ))}
+        </div>
+      </a>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,19 @@ import { repositorys } from '@/data/repositorys'
 
 const Home = () => {
   return (
-    <main className="w-full">
-      <header>
-        <h1>just4fun/just4now</h1>
+    <main className="p-4 flex flex-col gap-4 sm:justify-center items-center sm:h-screen">
+      <header className="flex flex-col gap-7 justify-center items-center">
+        <h1 className="text-5xl opacity-80">Terminal Tools</h1>
         <em>
-          <span>i like Terminal</span>
+          <span>『 - i like Terminal - 』</span>
         </em>
       </header>
-      <section>
-        <em>
-          <h2>Repositories</h2>
-        </em>
-
-        {repositorys.map((repository) => (
-          <RepositoryIntroCard key={repository.name} repository={repository} />
-        ))}
+      <section className="flex flex-col justify-center items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+          {repositorys.map((repository) => (
+            <RepositoryIntroCard key={repository.name} repository={repository} />
+          ))}
+        </div>
       </section>
       <footer></footer>
     </main>
